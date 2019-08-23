89a3954f2fca94d442f7e9c0b35b115625fec852
skyparci <skyparci@gmail.com>
2019-08-23 19:22:12 +0900

    Connecting the billing form

--- src/components/BillingForm.js
+++ src/components/BillingForm.js
@@ -2,94 +2,96 @@ import React, { Component } from 'react';
 import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 import { CardElement, injectStripe } from 'react-stripe-elements';
 import LoaderButton from './LoaderButton';
-import './BillingForm.css';
 
 class BillingForm extends Component {
     constructor(props) {
-        super(props) {
-            this.state = {
-                name: '',
-                storage: '',
-                isProcessing: false,
-                isCardComplete: false
-            };
-        }
+        super(props);
 
-        validateForm() {
-            return (
-                this.state.name !== '' &&
-                this.state.storage !== '' &&
-                this.state.isCardComplete
-            );
-        }
+        this.state = {
+            name: '',
+            storage: '',
+            isProcessing: false,
+            isCardComplete: false
+        };
+    }
 
-        handleFieldChange = event => {
-            this.setState({
-                [event.target.id]: event.target.value
-            });
-        }
+    validateForm() {
+        return (
+            this.state.name !== '' &&
+            this.state.storage !== '' &&
+            this.state.isCardComplete
+        );
+    }
 
-        handleCardFieldChange = event => {
-            this.setState({
-                isCardComplete: event.complete
-            });
-        }
+    handleFieldChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+        });
+    };
 
-        handleSubmitClick = async event => {
-            event.preventDefault();
+    handleCardFieldChange = event => {
+        this.setState({
+            isCardComplete: event.complete
+        });
+    };
 
-            const { name } = this.state;
-            this.setState({ isProcessing: true });
+    handleSubmitClick = async event => {
+        event.preventDefault();
 
-            const { token, error } = await this.props.stripe.createToken({ name });
-            this.setState({ isProcessing: false });
-            this.props.onSubmit(this.state.storage, { token, error });
-        }
+        const { name } = this.state;
+        this.setState({ isProcessing: true });
 
-        this.render() {
-            const loading = this.state.isProcessing || this.props.loading;
-            return (
-                <form className="BillingForm" onSubmit={this.handleSubmitClick}>
-                    <FormGroup bsSize="large" controlId="storage">
-                        <ControlLabel>Storage</ControlLabel>
-                        <FormControl
-                            min="0"
-                            type="number"
-                            value={this.state.storage}
-                            onChange={this.handleFieldChange}
-                            placeholder="Number of notes to store"
-                        />
-                    </FormGroup>
-                    <hr />
-                    <FormGroup bsSize="large" controlId="name">
-                        <ControlLabel>Cardholder&apos;s name</ControlLabel>
-                        <FormControl
-                            type="text"
-                            value={this.state.name}
-                            onChange={this.handleFieldChange}
-                            placeholder="Name on the card"
-                        />
-                    </FormGroup>
-                    <ControlLabel>Credit Card Info</ControlLabel>
-                    <CardElement
-                        className="card-field"
-                        onChange={this.handleCardFieldChange}
-                        style={{
-                            base: { fontSize: "18px", fontFamily: '"Open Sans", sans-serif' }
-                        }}
+        const { token, error } = await this.props.stripe.createToken({ name });
+        this.setState({ isProcessing: false });
+        this.props.onSubmit(this.state.storage, { token, error });
+    };
+
+    render() {
+        const loading = this.state.isProcessing || this.props.loading;
+        return (
+            <form className="BillingForm" onSubmit={this.handleSubmitClick}>
+                <FormGroup bsSize="large" controlId="storage">
+                    <ControlLabel>Storage</ControlLabel>
+                    <FormControl
+                        min="0"
+                        type="number"
+                        value={this.state.storage}
+                        onChange={this.handleFieldChange}
+                        placeholder="Number of notes to store"
                     />
-                    <LoaderButton
-                        block
-                        bsSize="large"
-                        type="submit"
-                        text="Purchase"
-                        isLoading={loading}
-                        loadingText="Purchasing…"
-                        disabled={!this.validateForm()}
+                </FormGroup>
+                <hr />
+                <FormGroup bsSize="large" controlId="name">
+                    <ControlLabel>Cardholder&apos;s name</ControlLabel>
+                    <FormControl
+                        type="text"
+                        value={this.state.name}
+                        onChange={this.handleFieldChange}
+                        placeholder="Name on the card"
                     />
-                </form>
-            );
-        }
+                </FormGroup>
+                <ControlLabel>Credit Card Info</ControlLabel>
+                <CardElement
+                    className="card-field"
+                    onChange={this.handleCardFieldChange}
+                    style={{
+                        base: {
+                            fontSize: '18px',
+                            fontFamily: '"Open Sans", sans-serif'
+                        }
+                    }}
+                />
+                <LoaderButton
+                    block
+                    bsSize="large"
+                    type="submit"
+                    text="Purchase"
+                    isLoading={loading}
+                    loadingText="Purchasing…"
+                    disabled={!this.validateForm()}
+                />
+            </form>
+        );
     }
 }
 
